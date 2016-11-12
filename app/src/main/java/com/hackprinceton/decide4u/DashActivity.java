1b00579704f1cad587115be71d66a3cdbaf3a390
Roland Fong <rfblue2@gmail.com>
2016-11-11 22:19:40 -0500

    Initial Commit

---
+++ app/src/main/java/com/hackprinceton/decide4u/DashActivity.java
@@ -0,0 +1,16 @@
+package com.hackprinceton.decide4u;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Displays your questions
+ */
+public class DashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash);
+    }
+}
---
+++ app/src/main/java/com/hackprinceton/decide4u/FeedActivity.java
@@ -0,0 +1,16 @@
+package com.hackprinceton.decide4u;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Displays other people's questions
+ */
+public class FeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+}
