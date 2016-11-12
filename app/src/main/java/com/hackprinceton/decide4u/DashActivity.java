1fdfd07047145f1a7f032c4f158635e0312c656b
Matt Wang <mattwang1997@gmail.com>
2016-11-12 15:55:06 -0500

    Adding to Dash

--- app/src/main/java/com/hackprinceton/decide4u/DashActivity.java
+++ app/src/main/java/com/hackprinceton/decide4u/DashActivity.java
@@ -1,16 +1,58 @@
 package com.hackprinceton.decide4u;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.hackprinceton.decide4u.model.Question;
+
+import java.util.ArrayList;
 
 /**
  * Displays your questions
  */
-public class DashActivity extends AppCompatActivity {
+public class DashActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private Button newDec;
+    private ListView myQuestions;
+    private ArrayList<Question> qList;
+    public final static String QUESTION_KEY = "com.example.feedactivity.QUESTION";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash);
+
+        newDec = (Button) findViewById(R.id.new_dec);
+        newDec.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(DashActivity.this, QuestionActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        myQuestions = (ListView) findViewById(R.id.my_questions);
+        myQuestions.setOnItemClickListener(this);
+
+        qList = new ArrayList<Question>();
+
+        qList.add(new Question("Which car?", "Honda Civic", "Toyota Camry", "Description"));
+        qList.add(new Question("What phone OS should I use?", "Android", "iOS", "Description"));
+        qList.add(new Question("Which class?", "COS 226", "COS 217", "Description"));
+
+        CustomListAdapter adapter = new CustomListAdapter(this, qList);
+        myQuestions.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+        String questionStr = (String) parent.getItemAtPosition(i).toString();
+        Intent intent = new Intent(DashActivity.this, QDetailActivity.class);
+        intent.putExtra(QUESTION_KEY, questionStr);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/hackprinceton/decide4u/DashListAdapter.java
@@ -0,0 +1,52 @@
+package com.hackprinceton.decide4u;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.hackprinceton.decide4u.model.Question;
+
+import java.util.ArrayList;
+
+/**
+ * Created by RotomPlasma on 11/12/16.
+ */
+
+public class DashListAdapter extends ArrayAdapter<Question> {
+    private Context qContext;
+    private LayoutInflater qInflater;
+    private ArrayList<Question> qDataSource;
+
+    public DashListAdapter(Context context, ArrayList<Question> questions) {
+        super(context, 0, questions);
+        qDataSource = questions;
+        qInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    static class ViewHolder {
+        TextView questionTextView;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        CustomListAdapter.ViewHolder holder;
+
+        // if an existing view is not being reused, inflate the view
+        if (convertView == null) {
+            holder = new CustomListAdapter.ViewHolder();
+            convertView = qInflater.inflate(R.layout.dashlist_elem_layout, parent, false);
+            holder.questionTextView = (TextView) convertView.findViewById(R.id.questionTitle);
+            convertView.setTag(holder);
+        } else {
+            holder = (CustomListAdapter.ViewHolder) convertView.getTag();
+        }
+
+        Question questionTitle = getItem(position);
+        holder.questionTextView.setText(questionTitle.toString());
+
+        return convertView;
+    }
+}
