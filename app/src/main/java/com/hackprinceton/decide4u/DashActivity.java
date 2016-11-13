3f3df5eda673107743ed33cc868a560a6a150775
Matt Wang <mattwang1997@gmail.com>
2016-11-12 21:15:11 -0500

    Attempting to add navmenu

--- app/src/main/java/com/hackprinceton/decide4u/DashActivity.java
+++ app/src/main/java/com/hackprinceton/decide4u/DashActivity.java
@@ -1,10 +1,14 @@
 package com.hackprinceton.decide4u;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -17,10 +21,13 @@ import java.util.ArrayList;
  */
 public class DashActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
+    public final static String QUESTION_KEY = "com.example.feedactivity.QUESTION";
     private Button newDec;
     private ListView myQuestions;
     private ArrayList<Question> qList;
-    public final static String QUESTION_KEY = "com.example.feedactivity.QUESTION";
+    private DrawerLayout dLayout;
+    private ListView menuOpts;
+    private String[] menuList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,20 +46,37 @@ public class DashActivity extends AppCompatActivity implements AdapterView.OnIte
         myQuestions.setOnItemClickListener(this);
 
         qList = new ArrayList<Question>();
-
         qList.add(new Question("Which car?", "Honda Civic", "Toyota Camry", "Description"));
         qList.add(new Question("What phone OS should I use?", "Android", "iOS", "Description"));
         qList.add(new Question("Which class?", "COS 226", "COS 217", "Description"));
 
-        CustomListAdapter adapter = new CustomListAdapter(this, qList);
+        DashListAdapter adapter = new DashListAdapter(this, qList);
         myQuestions.setAdapter(adapter);
+
+        dLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        menuOpts = (ListView) findViewById(R.id.drawer_opts);
+        menuList = new String[] {"Answer Questions", "My Questions"};
+
+        menuOpts.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, menuList));
+//        menuOpts.setOnItemClickListener(new DrawerItemClickListener());
+
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
-        String questionStr = (String) parent.getItemAtPosition(i).toString();
-        Intent intent = new Intent(DashActivity.this, QDetailActivity.class);
-        intent.putExtra(QUESTION_KEY, questionStr);
-        startActivity(intent);
+        new AlertDialog.Builder(this)
+                .setTitle("Invalid Entry")
+                .setMessage("Parent: " + parent)
+                .setNegativeButton("Back", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+//            String questionStr = (String) parent.getItemAtPosition(i).toString();
+//            Intent intent = new Intent(DashActivity.this, QDetailActivity.class);
+//            intent.putExtra(QUESTION_KEY, questionStr);
+//            startActivity(intent);
     }
 }
