373dad4035456ab5d1ef6e927c3da2e785309361
Sherry Bai <sherr@DESKTOP-AQLM9BN.(none)>
2016-11-13 06:39:39 -0500

    Style changes, progress bar updates

--- app/src/main/java/com/hackprinceton/decide4u/FeedActivity.java
+++ app/src/main/java/com/hackprinceton/decide4u/FeedActivity.java
@@ -1,8 +1,12 @@
 package com.hackprinceton.decide4u;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -26,11 +30,13 @@ public class FeedActivity extends AppCompatActivity implements AdapterView.OnIte
     public FeedActivity() {
     }
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
